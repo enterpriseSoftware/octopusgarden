1a069041d8ebec10a327259c870c85f383d21a61
Tim Kelley <abelwebsolutions@gmail.com>
2024-02-19 15:46:52 -0600

    update tomly file

--- functions/studio-ad.js
+++ functions/studio-ad.js
@@ -4,7 +4,7 @@ const postmark = require("postmark");
 exports.handler = async (event, context) => {
   const method = event.httpMethod;
   console.log(`http method = ${method}`);
-  
+
   if (method !== "POST") {
     return {
       statusCode: 405,
@@ -22,27 +22,30 @@ exports.handler = async (event, context) => {
   }
 
   const { EMAIL_API_KEY, EMAIL_TO } = process.env;
-  
+
   var client = new postmark.ServerClient(EMAIL_API_KEY);
 
   // Promisify the sendEmail call
   try {
     await new Promise((resolve, reject) => {
-      client.sendEmail({
-        From: email,
-        To: EMAIL_TO,
-        Subject: `${subject} from ${name}`,
-        HtmlBody: `<p>${message}</p>`,
-        MessageStream: "outbound",
-      }, (error, result) => {
-        if (error) {
-          console.error("Unable to send via Postmark: " + error.message);
-          reject(error);
-        } else {
-          console.info("Sent to Postmark for delivery");
-          resolve(result);
+      client.sendEmail(
+        {
+          From: email,
+          To: EMAIL_TO,
+          Subject: `${subject} from ${name}`,
+          HtmlBody: `<p>${message}</p>`,
+          MessageStream: "outbound",
+        },
+        (error, result) => {
+          if (error) {
+            console.error("Unable to send via Postmark: " + error.message);
+            reject(error);
+          } else {
+            console.info("Sent to Postmark for delivery");
+            resolve(result);
+          }
         }
-      });
+      );
     });
     return {
       statusCode: 200,
